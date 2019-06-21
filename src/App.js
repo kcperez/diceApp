42ea37f93e41d29b72fd6d34c93a20c59e015c64
kevin <kevin@whiteblock.io>
2019-06-20 20:42:01 -0700

    dice app demo in ReactJS

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Die from './Die';
+import RollDice from './RollDice';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <RollDice />
+
+    </div>
+  );
+}
+
+export default App;
